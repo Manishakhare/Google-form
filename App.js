142e1aa4a67af3167cb199fa4f3ea6830ee74c80
Manishakhare <93837426+Manishakhare@users.noreply.github.com>
2022-01-12 17:35:03 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,225 @@
+
+
+import './App.css';
+
+
+
+ 
+
+ 
+function App() {
+ 
+  return (
+    <div className="App">
+
+     
+      <div class="container">
+ 
+      <div class="content-wrapper">
+   
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+         
+          <div class="col-sm-12">
+          <div class="alert alert-info" role="alert">
+          <h1 class="">B tech S1 Exminations April 2018 -Supplementry-Common  To All Branches </h1>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>       
+  
+
+<table class="table" >
+
+
+  <thead class="thead-dark" Border="2px">
+   
+
+    <tr>
+   
+      <th scope="col">#</th>
+      <th scope="col">Date</th>
+      <th scope="col">Day</th>
+      <th scope="col">Time</th>
+      <th scope="col">Exam Slot</th>
+      <th scope="col">Course Code</th>
+      <th scope="col">Course Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td >02-05-2018</td>
+      <td>Wed</td>
+      <td>9:30-12:30</td>
+      <td>B</td>
+      <td>CY100</td>
+      <td>Engineering Chemistry</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>04-05-2018</td>
+      <td>Fri</td>
+      <td>9:30-12:30</td>
+      <td>B</td>
+      <td>PH100</td>
+      <td>Engineering Physics</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>08-05-2018</td>
+      <td>Tue</td>
+      <td>9:30-12:30</td>
+      <td>C</td>
+      <td>BE100</td>
+      <td>Engineering Mechanics</td>
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>10-05-2018</td>
+      <td>The</td>
+      <td>9:30-12:30</td>
+      <td>C</td>
+      <td>BE100</td>
+      <td>Engineering Graphics</td>
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>16-05-2018</td>
+      <td>Wed</td>
+      <td>9:30-12:30</td>
+      <td>E1</td>
+      <td>CE100</td>
+      <td> Basic of Engineering Civil Engineering</td>
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>18-05-2018</td>
+      <td>Fri</td>
+      <td>9:30-12:30</td>
+      <td>E2</td>
+      <td>ME100</td>
+      <td> Basic of Engineering Mechanical Engineering</td>
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>22-05-2018</td>
+      <td>Tue</td>
+      <td>9:30-12:30</td>
+      <td>F1</td>
+      <td>EE100</td>
+      <td> Basic of Electrical Engineering</td>
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>24-05-2018</td>
+      <td>Thu</td>
+      <td>9:30-12:30</td>
+      <td>F2</td>
+      <td>EC100</td>
+      <td> Basic of Electronics Engineering</td>
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>29-05-2018</td>
+      <td>Tue</td>
+      <td>9:30-12:30</td>
+      <td>E</td>
+      <td>BE103</td>
+      <td>Introduction to Sustainable Engineering</td>
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td>31-05-2018</td>
+      <td>Thu</td>
+      <td>9:30-12:30</td>
+      <td>D</td>
+      <td>BE101-01</td>
+      <td> Introduction  to  Civil  Engineering</td>
+    </tr>
+    <tr>
+      <th scope="row">11</th>
+      <td>31-05-2018</td>
+      <td>Thu</td>
+      <td>9:30-12:30</td>
+      <td>D</td>
+      <td>BE101-02</td>
+      <td> Introduction  to  Mechanical  Engineering Sciences</td>
+    </tr>
+    <tr>
+      <th scope="row">12</th>
+      <td>31-05-2018</td>
+      <td>Thu</td>
+      <td>9:30-12:30</td>
+      <td>D</td>
+      <td>BE101-03</td>
+      <td> Introduction  to  Electrical  Engineering</td>
+    </tr>
+    <tr>
+      <th scope="row">13</th>
+      <td>31-05-2018</td>
+      <td>Thu</td>
+      <td>9:30-12:30</td>
+      <td>D</td>
+      <td>BE101-04</td>
+      <td> Introduction  to  Electronics  Engineering</td>
+    </tr>
+    <tr>
+      <th scope="row">14</th>
+      <td>31-05-2018</td>
+      <td>Thu</td>
+      <td>9:30-12:30</td>
+      <td>D</td>
+      <td>BE101-05</td>
+      <td> Introduction  to  Computing  and Problem Solving   Engineering</td>
+    </tr>
+    <tr>
+      <th scope="row">15</th>
+      <td>31-05-2018</td>
+      <td>Thu</td>
+      <td>9:30-12:30</td>
+      <td>D</td>
+      <td>BE101-06</td>
+      <td> Introduction  to  Chemical   Engineering</td>
+    </tr>
+    <tr>
+      <th scope="row">16</th>
+      <td>04-05-2018</td>
+      <td>Mon</td>
+      <td>9:30-12:30</td>
+      <td>A</td>
+      <td>MA101</td>
+      <td>Calculus</td>
+    </tr>
+
+  </tbody>
+</table>
+</div>
+      {/* <Nav/> */}
+    
+      {/* <Fetch/> */}
+
+      {/* <Body/>
+      <F /> */}
+    
+
+    
+{/* <Fetch/> */}
+{/* <Fetch/> */}
+
+     </div>
+     </div>
+  
+     
+     );
+
+     
+};
+
+
+
+export default App;
+
+// export default App1;
